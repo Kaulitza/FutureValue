64f7dd2e7b77b32d491f195707d7cef852a1dec5
Michelle Vanwettere <57185740+Kaulitza@users.noreply.github.com>
2020-09-08 17:36:49 -0400

    Add files via upload

--- Models/FutureValueModel.cs
+++ Models/FutureValueModel.cs
@@ -1,10 +1,20 @@
-﻿namespace FutureValue.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace FutureValue.Models
 {
     public class FutureValueModel
     {
+        [Required(ErrorMessage = "Please enter a monthly investment.")]
+        [Range(1,500, ErrorMessage = "Monthly investment amount must be between 1 and 500.")]
         public decimal MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Please enter an interest rate.")]
+        [Range(0.1, 10.0, ErrorMessage = "Interest rate must be between 0.1 and 10.0")]
         public decimal YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number of years.")]
+        [Range(1, 50, ErrorMessage = "Number of years must be between 1 and 50.")]
         public int Years { get; set; }
+
         public decimal CalculateFutureValue()
         {
             int months = Years * 12;
